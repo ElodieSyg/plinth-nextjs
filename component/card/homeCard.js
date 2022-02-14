d6738c3e17edf2e82ab69868ec18e16e451b809e
ElodieSayavong <elodie.sayavong.dev@gmail.com>
2022-02-14 10:28:45 +0100

    add homeCard.js components and container

---
+++ component/card/homeCard.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+// COMPONENTS IMPORTATION
+import GreenRoundedButton from "../layout/button/GreenRoundedButton";
+import GreenTitleItalic from "../layout/title/GreenTitleItalic";
+
+const Container = styled.div`
+    margin: 1rem;
+    display: flex;
+    justify-content: space-around;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+
+    :hover {
+        background: #F7F9F8;
+    };
+`;
+
+const ImageContainer = styled.div`
+    background: yellow;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5rem;
+    margin: 1rem;
+`;
+
+const InfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 0.5rem;
+    margin: 1rem;
+`;
+
+const Item = styled.div`
+    margin: 0.2rem;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 1rem;
+`;
+
+const HomeCard = (props) => {
+    return (
+        <Container>
+            <ImageContainer>
+                image à ajouter ici
+            </ImageContainer>
+            <InfoContainer>
+                <GreenTitleItalic textAlign="center">{props.title}</GreenTitleItalic>
+                <Item>
+                    Category : {props.category}
+                </Item>
+                <Item>
+                    Description : {props.description}
+                </Item>
+                <Item>
+                    État : {props.status}
+                </Item>
+            </InfoContainer>
+            <ButtonContainer>
+                <GreenRoundedButton>Voir plus</GreenRoundedButton>
+            </ButtonContainer>
+        </Container>
+    );
+};
+
+export default HomeCard;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import { useEffect, useState } from "react";
+import { server } from "../tools";
+import axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
 import styled from "styled-components";
 // COMPONENTS IMPORTATION
 import Navbar from "../component/layout/navbar/navbar";
+import HomeCard from "../component/card/homeCard";
 // STYLED COMPONENTS IMPORTATION
 import BlackTitle from "../component/layout/title/BlackTitle";
 import GlobalContainer from "../styles/styled-components/container/GlobalContainer";
@@ -12,6 +16,10 @@ const AboutContainer = styled.div`
   flex-direction: column;
   align-items: center;
   border-bottom: 1px solid #f0f0f0;
+`;
+
+const CardContainer = styled.div`
+  background: red;
 `;
 
 const Item = styled.div`
@@ -25,6 +33,15 @@ const GreySmallText = styled.p`
 `;
 
 const Home = () => {
+  const [products, setProducts] = useState();
+
+/*   useEffect(() => {
+    axios.get(`${server}/api/product`, { withCredentials: true })
+      .then(res => {
+        console.log("result", res);
+      });
+  }, []); */
+
   return (
     <>
       <Head>
@@ -47,6 +64,9 @@ const Home = () => {
           </Item>
         </AboutContainer>
       </GlobalContainer>
+      <CardContainer>
+        Add home card
+      </CardContainer>
     </>
   );
 };
